2ad8fa61293d061e18b91b239196b0ed9135dabe
Namisato <namisato.58@gmail.com>
2025-05-10 11:35:23 +0900

    コミット

---
+++ script.js
@@ -0,0 +1,7 @@
+// ハンバーガーメニューの開閉処理
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
